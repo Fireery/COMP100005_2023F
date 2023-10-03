3b090250ba2d58cc3258e1c0345b4587891ddf66
Fireery <51524894+Fireery@users.noreply.github.com>
2023-10-03 12:03:09 -0400

    Add files via upload

---
+++ Week2_Ex/Week2_Ex/Program.cs
@@ -0,0 +1,17 @@
+﻿//Ex-1
+string firstName;
+char middleInitial;
+string lastName;
+
+Console.WriteLine("Enter your first name:");
+firstName = Console.ReadLine();
+Console.WriteLine("Enter your middle initial:");
+middleInitial = Convert.ToChar(Console.ReadLine());
+Console.WriteLine("Enter your last name:");
+lastName = Console.ReadLine();
+
+Console.WriteLine($"{firstName}{lastName}");
+Console.WriteLine($"{firstName}{middleInitial}{lastName}");
+Console.WriteLine($"{lastName}{firstName}{middleInitial}");
+
+//Ex-2
